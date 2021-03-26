3d9729d57d6e9e41253e621af70371ff0b6f73d4
Blaise Barré <blaise.barre@gmail.com>
2021-03-26 09:20:24 +0100

    try storage

--- src/screens/book/index.js
+++ src/screens/book/index.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import Header from "../../composants/Header";
+import getBooks from "../../../models/books";
+import { getData, storeData } from '../../../utils/StoreManager';
 
 export default function book({route, navigation}) {
     const { item } = route.params ;
+    const [ favoris, setFavoris] = useState([]);
+
+    useEffect(() => {
+        async function getFavoris()  {
+            let favoris = await getData('favoris_book_'+item.id) ;
+            setFavoris(favoris);
+        }
+        getFavoris() ;
+    }, []);
+
+    const clicFavoris = async (favoris) => {
+        alert('ok');
+        const key = 'favoris_book_' + item.id ;
+        await storeData(key, favoris) ;
+        //setFavoris(favoris);
+    }
 
     return (
         <View style={styles.container}>
@@ -15,6 +33,7 @@ export default function book({route, navigation}) {
                 <Text>{item.fields["Nom"]}</Text>
                 <Text>{item.fields["Description"]}</Text>
                 <Text>Auteur : {item.fields["Nom complet (from Auteur)"]}</Text>
+                <TouchableOpacity onPress={() => { clicFavoris(!favoris)}}><Text>{favoris ? 'Ne plus mettre en favoris' : 'Mettre en favoris'}</Text></TouchableOpacity>
                 {item.fields["Photo (from Auteur)"] !== undefined && item.fields["Photo (from Auteur)"].length > 0 && <Image source={{uri: item.fields["Photo (from Auteur)"][0]["url"]}} style={{ width: '100%', height: '100%'}} />}
             </View>
         </View>
---
+++ utils/StoreManager.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (key, value) => {
+    try {
+        alert('ok');
+        await AsyncStorage.setItem('@'+key, value)
+    } catch (e) {
+        // saving error
+    }
+}
+
+export const getData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem('@' + key)
+    if(value !== null) {
+      // value previously stored
+    }
+  } catch(e) {
+    // error reading value
+  }
+}
+
